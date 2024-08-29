884d1105f6da20718998bd6db0aefa242b0242cb
Utbhab <utbhab.debnath01@gmail.com>
2024-08-29 23:57:09 +0530

    Add Project files

--- public/js/map.js
+++ public/js/map.js
@@ -1,11 +1,11 @@
     mapboxgl.accessToken = mapToken;
     const map = new mapboxgl.Map({
         container: 'map', // container ID
-        center: listing.geomatry.coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
+        center: listing.geometry.coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
         zoom: 9 // starting zoom
     });
 
     const marker = new mapboxgl.Marker({color: red})
-    .setLngLat(listing.geomatry.coordinates)
+    .setLngLat(listing.geometry.coordinates)
     .setPopup(new mapboxgl.Popup({offset: 25, className: 'my-class'}).setHTML(`<h4>${listing.location}</h4>`))
     .addTo(map);
